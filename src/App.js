7d5e21cb71ecc90e7a15673748c1a7fd23bcbecd
itay taasa <itaytaasa@gmail.com>
2021-01-10 10:38:22 +0200

    nfirst draft

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
 import React from 'react';
 import './App.css';
+import Forecast1Day from './Forecast1Day/Forecast1Day';
 
 class App extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            forecast: []
+        }
+    }
+
+    componentDidMount() {
+        fetch('https://netcraft2.s3-eu-west-1.amazonaws.com/weather.json')
+            .then(res => res.json())
+            .then((days) => {
+                console.log(days)
+                this.setState({ forecast: days })
+            })
+    }
+    
+
 
     render() {
+        // let background= "yellow",
         return (
-            <div className="App">
-                Your code here
+            <div className="App" style={{background:"lightblue"}}>
+                {this.state.forecast.map((day,index)=> {
+                    return <Forecast1Day key={index} day={day.day} temperature={day.temperature} description={day.description} icon={day.icon} />
+                })}
+               
             </div>
         );
     }
---
+++ src/Forecast1Day/Forecast1Day.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+
+import './Forcast1Day.css';
+export default class Forecast1Day extends Component {
+    constructor(props) {
+        super(props)
+
+
+    }
+
+
+    changeToDay(num) {
+        let day;
+        switch (num) {
+            case 0:
+                day = "sunday"
+                break;
+            case 1:
+                day = "Monday"
+                break;
+            case 2:
+                day = "Tuesday"
+                break;
+            case 3:
+                day = "Wednesday"
+                break;
+            case 4:
+                day = "Thursday"
+                break;
+            case 5:
+                day = "Friday"
+                break;
+            case 6:
+                day = "Saturday"
+            default:
+
+        }
+        return day
+    }
+
+
+
+    render() {
+        return (
+            <div className="single-day">
+                <p className="day-name">{this.changeToDay(this.props.day)}</p>
+                <img src={require('../icons/' + this.props.icon + ".png")} alt="" />
+                <p className="temp"> <span className="degrees" >{this.props.temperature}&#176;</span><span className="description">{this.props.description}</span></p>
+
+            </div>
+        );
+    }
+}
