c1f84383fc284c77efd2a4f84c95456797497be8
itay taasa <itaytaasa@gmail.com>
2021-01-11 11:57:57 +0200

    added Sound effects

--- src/Forecast1Day/Forecast1Day.js
+++ src/Forecast1Day/Forecast1Day.js
@@ -1,20 +1,19 @@
 import React, { Component } from 'react';
+import Sounds1 from '../Sounds/18.mp3';
+import Sounds2 from '../Sounds/1.mp3';
 
 import './Forcast1Day.css';
 export default class Forecast1Day extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            showAnimation: false,
+            isSoundOn: false
         }
 
     }
 
 
-    handleBoxToggle(e) {
-        this.props.onAddAnimaion()
 
-    }
 
     changeToDay(num) {
         let day;
@@ -44,12 +43,29 @@ export default class Forecast1Day extends Component {
         }
         return day
     }
+    changeSound(icon) {
+       
+        this.setState({ isSoundOn: !this.state.isSoundOn })
+        console.log(typeof icon)
+        if(icon==="18"){
+          
+            let audio = new Audio(Sounds1)
+            audio.play()
+        }
+        if(icon==="1"){
+       
+            let audio = new Audio(Sounds2)
+            audio.play()
+        }
 
+
+    }
 
 
     render() {
+
         return (
-            <div className={this.props.className}>
+            <div className={this.props.className} onMouseEnter={this.changeSound.bind(this, this.props.icon)}>
                 <p className="day-name">{this.changeToDay(this.props.day)}</p>
                 <img src={require('../icons/' + this.props.icon + ".png")} alt="" />
                 <p className="temp"> <span className="degrees" >{this.props.temperature}&#176;</span><span className="description">{this.props.description}</span></p>
